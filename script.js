71bf192b64f8af8de1ff866a361e66c10f86b436
Emily Wu <embrilliant@hotmail.com>
2015-04-11 18:03:47 +0800

    half trans

--- script.js
+++ script.js
@@ -17,27 +17,42 @@ $(function() {
 	c.fillStyle = "#c6c6c6";
 	c.fillText("Brush here :) ", 225, 150); */
 	
-	c.drawImage(img, 0, 0, 640, 360);
-	
-	function drawVideo() {
-		// vid.play();
-		// setInterval(function() {
-			c.drawImage(img, 0, 0, 640, 360);
-		// }, 0);
+	// c.drawImage(img, 0, 0, 640, 360);
 
-	}
+	$(window).on("mousedown", function() {  
+		vid.play();
+		runAnalysis();
+	});
+
+	//  $("window").on("click", function() {
+	// 	vid.play();
+	// 	brush();
+	// });
+	/* 
+	vid.play();
+	setInterval(function() {
+		c.drawImage(vid, 0, 0, 640, 360);
+		var image = c.getImageData(0, 0, canvasWidth, canvasHeight);
+		cOutput.putImageData(image, 0, 0, 0, 0, canvasWidth, canvasHeight);
+		manip();
+		brush();
+	}, 0);
+	*/
+	// function drawVideo() {
+	// 	c.drawImage(vid, 0, 0, 640, 360);
+	// }
 
 	var newColr = {r: 0, g: 0, b: 0, a: 0};
 
+	var image = c.getImageData(0, 0, canvasWidth, canvasHeight);
+
 	function brush(event) { 
-		
+
 		var $this = $(this);
-		
-		// vid.play();
 		
 		$this.on({ 	// Attach multiple event handlers simultaneously using a plain object. (http://api.jquery.com/on/#on-events-selector-data)
 			mousemove: function(event) {
-
+				
 				// get brush position
 				var xPos = event.pageX, // left                   
 					yPos = event.pageY;	// top
@@ -54,7 +69,7 @@ $(function() {
 				c.arc(brushPosX, brushPosY, 30, 0, 2 * Math.PI, false);
 				c.fill();
 
-				manip();
+				// manip();
 			},
 			mouseup: function() {
 				$this.off("mousemove");
@@ -63,6 +78,7 @@ $(function() {
 	}
 
 	function manip() {
+		c.drawImage(vid, 0, 0, 640, 360);
 		var image = c.getImageData(0, 0, canvasWidth, canvasHeight),
 		imageData = image.data,
 		length = imageData.length;
@@ -72,11 +88,11 @@ $(function() {
 				b = imageData[i+2],
 				a = imageData[i+3]; 
 			
-			if (r == 0 && g == 0 && b == 0) {
-				imageData[i] = newColr.r;
-	    		imageData[i+1] = newColr.g;
-				imageData[i+2] = newColr.b;
-	            imageData[i+3] = newColr.a;
+			if (r > 0 && g > 0 && b > 0) {
+				// imageData[i] = 0;
+	 	 		// imageData[i+1] = 0;
+				// imageData[i+2] = 0;
+	            imageData[i+3] = 100;
 			}
 		}
 
@@ -85,20 +101,11 @@ $(function() {
 		cOutput.putImageData(image, 0, 0, 0, 0, canvasWidth, canvasHeight);
 	}
 
-	$(window).on("mousedown", function() {  
-		// drawVideo();
-		brush(); 
-	});
-	/* $("window").on("click", function() {
-		vid.play();
-		runAnalysis();
-	}); */
-
 	function runAnalysis() {
 	    if (vid.paused || vid.ended) {
 	    	return;
 	    }
-    	frameConversion();
+    	manip();
     	if (requestAnimationFrame) {
         	requestAnimationFrame(runAnalysis);
         } else {
@@ -106,20 +113,20 @@ $(function() {
     	}
   	}
 
-  	function frameConversion() {
+  	function processFrame() {
   		c.drawImage(vid, 0, 0, 640, 360);
   		var frame = c.getImageData(0, 0, 640, 360);
   		var length = frame.data.length;
         for (var i = 0; i < length; i += 4) {
-        	var r = frame.data[0],
-            	g = frame.data[1],
-           		b = frame.data[2],
-           		a = frame.data[3];
+        	var r = frame.data[i],
+            	g = frame.data[i+1],
+           		b = frame.data[i+2];
 
-	        if(r == 0 && g == 255 && b == 0) {  
-	        	a = 0; 
+	        if (r > 0 && g > 0 && b > 0) {  
+	        	frame.data[i+3] = 10; 
 	        } 
         }
+
     	cOutput.putImageData(frame, 0, 0);
   	}
 
